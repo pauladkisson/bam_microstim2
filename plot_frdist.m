54e34d0cce66877af49a12e1e7aded9ba306787f
pauladkisson <paul.wesley.adkisson@gmail.com>
2022-10-24 13:30:47 -0700

    refactored plot_frdist

--- plot_frdist.m
+++ plot_frdist.m
@@ -2,14 +2,12 @@
 %%% 2/14/2022
 %%% Plot Firing Rate Distribution over distance from electrode
 function plot_frdist(sim_name, ex_c, pulse_amps, stim_amps, t, num_group, num_affected, ...
-                     win_start, win_stop, default_colors, num_brains, ...
+                     win_start, win_stop, default_colors, ...
                      pulse_coherences, galvanic_coherences, control_coherences, ...
                      start_trial, end_trial, num_trials, plot_name)
-    stim_frs = zeros(length(stim_amps), num_group);
-    ball_rs = zeros(num_group, 1); 
+    stim_frs = zeros(length(stim_amps), num_trials, num_group);
     load(sprintf("Simulation %s/ustim/r.mat", sim_name), "ball_r")
-    ball_rs(1:num_affected) = ball_r;
-    ball_rs(num_affected+1:end) = 2500*1e-6;
+    ball_rs = get_ball_rs(ball_r, num_affected, num_group);
     for j = 1:length(stim_amps)
         stim_amp = stim_amps(j);
         pulse = j<=length(pulse_amps);
@@ -22,7 +20,6 @@ function plot_frdist(sim_name, ex_c, pulse_amps, stim_amps, t, num_group, num_af
                 [sim_name, stim_amp*1e6]);
             if stim_amp == 0
                 stim_coherences = control_coherences;
-                ctrl_ball_r = ball_r;
             else
                 stim_coherences = galvanic_coherences;
             end
@@ -30,15 +27,12 @@ function plot_frdist(sim_name, ex_c, pulse_amps, stim_amps, t, num_group, num_af
         if sim_name == "EMBC Disconnected"
             stim_coherences = 0;
         end
-        load(strcat(output_stimpath, "/decisions.mat"), "final_decisions")
+        load(strcat(output_stimpath, "/decisions.mat"), "decisions")
         if plot_name == "p1_wins"
-            num_wins = sum(final_decisions==1, 'all');
             analyze_coherences = stim_coherences;
         elseif plot_name == "p1_loses"
-            num_wins = sum(final_decisions==2, 'all');
             analyze_coherences = stim_coherences;
         else
-            num_wins = num_trials;
             analyze_coherences = ex_c;
         end
         for ex_c = analyze_coherences
@@ -46,6 +40,7 @@ function plot_frdist(sim_name, ex_c, pulse_amps, stim_amps, t, num_group, num_af
                 relative_trial = trial - start_trial + 1;
                 if (plot_name == "p1_wins" && final_decisions(relative_trial, stim_coherences==ex_c) ~= 1) || ...
                         (plot_name == "p1_loses" && final_decisions(relative_trial, stim_coherences==ex_c) ~= 2)
+                    stim_frs(j, trial, :) = NaN;
                     continue
                 end
                 load(strcat(output_stimpath, sprintf("/c=%0.3f/trial%0.0f.mat", [ex_c, trial])), ...
@@ -55,25 +50,22 @@ function plot_frdist(sim_name, ex_c, pulse_amps, stim_amps, t, num_group, num_af
                     spiketimes = t(recspikes(int2str(nn)));
                     g1_taskfrs(nn) = sum(spiketimes>=win_start & spiketimes<win_stop) / (win_stop - win_start);
                 end
-                stim_frs(j, :) = reshape(stim_frs(j, :), size(g1_taskfrs)) + g1_taskfrs ./ num_wins;
+                stim_frs(j, trial, :) = g1_taskfrs;
             end
         end
     end
-    ctrl_mean = mean(stim_frs(3, 1, :));
-    ctrl_std = std(stim_frs(3, 1, :));
-    %top_N = num_group * 0.2; %show top 20% closest to the electrode
-    top_N = num_group;
-    total_N = top_N;
-    pulse_frs = reshape(stim_frs(1, 1:top_N), [total_N, 1]);
-    galvanic_frs = reshape(stim_frs(2, 1:top_N), [total_N, 1]);
-    ctrl_frs = reshape(stim_frs(3, 1:top_N), [top_N, 1]);
+    pulse_frs = reshape(mean(stim_frs(1, :, :), 2, 'omitnan'), [num_group, 1]);
+    galvanic_frs = reshape(mean(stim_frs(2, :, :), 2, 'omitnan'), [num_group, 1]);
+    control_frs = reshape(mean(stim_frs(3, :, :), 2, 'omitnan'), [num_group, 1]);
+    ctrl_mean = mean(control_frs);
+    ctrl_std = std(control_frs);
 
     figure;
     set(gca, 'fontsize', 18)
     hold on
-    scatter(ball_rs*1e6, pulse_frs, [], ones(total_N, 3).*default_colors(7, :), 'filled')
-    scatter(ball_rs*1e6, galvanic_frs, [], ones(total_N, 3).*default_colors(5, :), 'filled')
-    scatter(ball_rs*1e6, ctrl_frs, [], "k", 'filled')
+    scatter(ball_rs*1e6, pulse_frs, [], ones(num_group, 3).*default_colors(7, :), 'filled')
+    scatter(ball_rs*1e6, galvanic_frs, [], ones(num_group, 3).*default_colors(5, :), 'filled')
+    scatter(ball_rs*1e6, control_frs, [], "k", 'filled')
     yline(ctrl_mean+3*ctrl_std, "k--")
     yline(ctrl_mean-3*ctrl_std, "k--") 
     hold off
@@ -84,63 +76,4 @@ function plot_frdist(sim_name, ex_c, pulse_amps, stim_amps, t, num_group, num_af
     else
         title("Connected")
     end
-
-    %trial/pop means
-    pulse_trialpop_mean = mean(reshape(stim_frs(1, :, :), [num_brains, num_group]), 2);
-    galvanic_trialpop_mean = mean(reshape(stim_frs(2, :, :), [num_brains, num_group]), 2);
-    pulse_norm_mean = pulse_trialpop_mean - ctrl_mean;
-    galvanic_norm_mean = galvanic_trialpop_mean - ctrl_mean;
-    stim_means = [mean(galvanic_norm_mean); mean(pulse_norm_mean)];
-    stim_sems = [std(galvanic_norm_mean)/sqrt(num_brains); std(pulse_norm_mean)/sqrt(num_brains)];
-
-    figure;
-    set(gca, 'fontsize', 18)
-    hold on
-    b = bar(stim_means);
-    b.FaceColor = 'flat';
-    b.CData = [default_colors(5, :); default_colors(7, :)];
-    plot([ones(1, num_brains); 2*ones(1, num_brains)], [galvanic_norm_mean'; pulse_norm_mean'], 'ko-')
-    hold off
-    xticks([1, 2])
-    xticklabels(["Galvanic", "Pulsatile"])
-    ylabel("Change in Firing Rate (spk/s)")
-    if sim_name == "EMBC Disconnected" || sim_name == "DepolBlockDiscon" || sim_name == "Test"
-        title("Disconnected")
-    else
-        title("Connected")
-    end
-
-    %{
-    % %affected Note: only considering increasing firing rate as affected
-    pulse_brainfrs = reshape(stim_frs(1, :, :), [num_brains, num_group]);
-    galvanic_brainfrs = reshape(stim_frs(2, :, :), [num_brains, num_group]);
-    pulse_affected = pulse_brainfrs > ctrl_mean + ctrl_std*3;
-    galvanic_affected = galvanic_brainfrs > ctrl_mean + ctrl_std*3;
-    pulse_percent_affected = sum(pulse_affected, 2) / size(pulse_affected, 2)
-    galvanic_percent_affected = sum(galvanic_affected, 2) / size(galvanic_affected, 2)
-    [~, p_perc_act] = ttest2(pulse_percent_affected, galvanic_percent_affected)
-
-    % %of neurons affected by pulse by also affected by galvanic
-    num_affected = 0;
-    for brain = brains
-        for nn = 1:num_group
-            if pulse_affected(brain, nn) && galvanic_affected(brain, nn)
-                num_affected = num_affected + 1;
-            end
-        end
-    end
-    percent_pulse_affected_affected_by_galvanic = num_affected / sum(pulse_affected, 'all');
-
-    %stats
-    [~, p_ttest] = ttest2(galvanic_norm_mean, pulse_norm_mean)
-    galvanic_norm_bar = mean(galvanic_norm_mean)
-    pulse_norm_bar = mean(pulse_norm_mean)
-    galvanic_norm_sem = std(galvanic_norm_mean) ./ sqrt(length(galvanic_norm_mean))
-    pulse_norm_sem = std(pulse_norm_mean) ./ sqrt(length(pulse_norm_mean))
-    pulse_trialbrainmean = mean(reshape(stim_frs(1, :, :), [num_brains, num_group]), 1);
-    galvanic_trialbrainmean = mean(reshape(stim_frs(2, :, :), [num_brains, num_group]), 1);
-    figure;
-    hold on
-    [~, p_kstest] = kstest2(pulse_frs, galvanic_frs)
-    %}
 end

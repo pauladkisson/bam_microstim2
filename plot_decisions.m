3b3c2d2a2d67f13a393308a58b67d0aae522f538
pauladkisson <paul.wesley.adkisson@gmail.com>
2023-04-17 15:47:04 -0700

    Added statistical analyses for decision-making metrics

--- plot_decisions.m
+++ plot_decisions.m
@@ -77,21 +77,40 @@ function plot_decisions(sim_name, pulse_amps, stim_amps, default_colors, ...
     legend("Pulsatile", "Galvanic", "Control", "Anodic")
     ylim([0, 1])
     
-    %Statistics
+    % Compute Psychometric Parameters
     a = reshape(stim_coeffs(:, :, 1), [length(stim_amps), num_batch]);
     b = reshape(stim_coeffs(:, :, 2), [length(stim_amps), num_batch]);
     bias = -a ./ b;
     sensitivity = b ./ 4;
 
-    avg_ps_bias = mean(bias(1, :))
-    sem_ps_bias = std(bias(1, :)) ./ sqrt(num_batch)
-    avg_gs_bias = mean(bias(2, :))
-    sem_gs_bias = std(bias(2, :)) ./ sqrt(num_batch)
-    avg_ctrl_bias = mean(bias(3, :))
-    sem_ctrl_bias = std(bias(3, :)) ./ sqrt(num_batch)
-    avg_an_bias = mean(bias(4, :))
-    sem_an_bias = std(bias(4, :)) ./ sqrt(num_batch)
+    % Bias Stats
+    avg_ps_bias = mean(bias(1, :));
+    sem_ps_bias = std(bias(1, :)) ./ sqrt(num_batch);
+    avg_gs_bias = mean(bias(2, :));
+    sem_gs_bias = std(bias(2, :)) ./ sqrt(num_batch);
+    avg_an_bias = mean(bias(4, :));
+    sem_an_bias = std(bias(4, :)) ./ sqrt(num_batch);
+    [~, p_cgs_ags] = ttest2(abs(bias(2, :)), abs(bias(4, :)));
+    [~, p_ps_cgs] = ttest2(bias(1, :), bias(2, :));
+    [~, p_ctrl] = ttest(bias(3, :));
+    disp("BIAS")
+    fprintf([...
+        'PS and CGS shifted psychometric curve by %0.1f +/- %0.1f%%', ...
+        ' and %0.1f +/- %0.1f%% respectively. \n'], ...
+        avg_ps_bias*100, sem_ps_bias*100, avg_gs_bias*100, sem_gs_bias*100)
+    fprintf('AGS shifted the psychometric curve by %0.1f +/- %0.1f%%. \n', ...
+            avg_an_bias*100, sem_an_bias*100)
+    fprintf([...
+        'Control did not significantly bias the psychometric curve ', ...
+        '(p = %0.2f). \n'], p_ctrl)
+    fprintf([...
+        'AGS shifted the psychometric curve significantly less than CGS ', ...
+        '(p=%0.2e). \n'], p_cgs_ags)
+    fprintf([...
+        'PS and CGS produced statistically identical bias (p=%0.2f). \n'], ...
+       p_ps_cgs)
 
+   % Sensitivity Stats
     ps_sens = reshape(sensitivity(1, :), [num_batch, 1]);
     gs_sens = reshape(sensitivity(2, :), [num_batch, 1]);
     ctrl_sens = reshape(sensitivity(3, :), [num_batch, 1]);
@@ -101,12 +120,30 @@ function plot_decisions(sim_name, pulse_amps, stim_amps, default_colors, ...
     norm_gs_sens = gs_sens - mean_ctrl_sens;
     norm_an_sens = an_sens - mean_ctrl_sens;
     
-    avg_norm_ps_sens = mean(norm_ps_sens)
-    avg_norm_gs_sens = mean(norm_gs_sens)
-    avg_norm_an_sens = mean(norm_an_sens)
-    sem_norm_ps_sens = std(norm_ps_sens) ./ sqrt(num_batch)
-    sem_norm_gs_sens = std(norm_gs_sens) ./ sqrt(num_batch)
-    sem_norm_an_sens = std(norm_an_sens) ./ sqrt(num_batch)
+    avg_norm_ps_sens = mean(norm_ps_sens);
+    avg_norm_gs_sens = mean(norm_gs_sens);
+    avg_norm_an_sens = mean(norm_an_sens);
+    sem_norm_ps_sens = std(norm_ps_sens) ./ sqrt(num_batch);
+    sem_norm_gs_sens = std(norm_gs_sens) ./ sqrt(num_batch);
+    sem_norm_an_sens = std(norm_an_sens) ./ sqrt(num_batch);
+    [~, ~, stats] = anova1(sensitivity', [], 'off');
+    results = multcompare(stats, 'Display', 'off');
+    p_vals = results(:, end);
+    p_ps = p_vals(2);
+    p_gs = p_vals(4);
+    p_ags = p_vals(end);
+    
+    disp("SENSITIVITY")
+    fprintf([...
+        'PS (p=%0.2f) and CGS (p=%0.2f) decreased the sensitivity ', ...
+        'by %0.2f +/- %0.2f and %0.2f +/- %0.2f respectively ', ...
+        'relative to control. \n'], p_ps, p_gs, avg_norm_ps_sens, ...
+        sem_norm_ps_sens, avg_norm_gs_sens, sem_norm_gs_sens)
+    
+    fprintf([...
+        'AGS (p=%0.2f) increased the sensitivity by %0.2f +/- %0.2f ', ...
+        'relative to control. \n'], ...
+        p_ags, avg_norm_an_sens, sem_norm_an_sens)
 
     
     %Decision Times
@@ -126,7 +163,7 @@ function plot_decisions(sim_name, pulse_amps, stim_amps, default_colors, ...
         elseif stim_amp < 0 %cathodic GS
             galvanic_dt = decision_times;
         elseif stim_amp == 0 %control
-            ctrl_dt = decision_times;
+            control_dt = decision_times;
         else %anoidc GS
             anodic_dt = decision_times;
         end
@@ -135,8 +172,8 @@ function plot_decisions(sim_name, pulse_amps, stim_amps, default_colors, ...
     trialstd_pulse = std(pulse_dt, [], 1, 'omitnan');
     trialmean_galvanic = mean(galvanic_dt, 1, 'omitnan');
     trialstd_galvanic = std(galvanic_dt, [], 1, 'omitnan');
-    trialmean_ctrl = mean(ctrl_dt, 1, 'omitnan');
-    trialstd_ctrl = std(ctrl_dt, [], 1, 'omitnan');
+    trialmean_ctrl = mean(control_dt, 1, 'omitnan');
+    trialstd_ctrl = std(control_dt, [], 1, 'omitnan');
     trialmean_anodic = mean(anodic_dt, 1, 'omitnan');
     trialstd_anodic = std(anodic_dt, 1, 'omitnan');
     figure;
@@ -156,6 +193,65 @@ function plot_decisions(sim_name, pulse_amps, stim_amps, default_colors, ...
     xlabel("Coherence (%)")
     ylabel("Decision Time (s)")
     legend("Pulsatile", "Galvanic", "Control", "Anodic")
+    
+    % DT stats
+    [ps_peak_dt, ps_peak_coh] = max(pulse_dt, [], 2, 'omitnan');
+    ps_peak_coh = pulse_coherences(ps_peak_coh);
+    [gs_peak_dt, gs_peak_coh] = max(galvanic_dt, [], 2, 'omitnan');
+    gs_peak_coh = galvanic_coherences(gs_peak_coh);
+    [ctrl_peak_dt, ctrl_peak_coh] = max(control_dt, [], 2, 'omitnan');
+    ctrl_peak_coh = control_coherences(ctrl_peak_coh); 
+    [an_peak_dt, an_peak_coh] = max(anodic_dt, [], 2, 'omitnan');
+    an_peak_coh = anodic_coherences(an_peak_coh);
+    avg_ctrl_peak_dt = mean(ctrl_peak_dt);
+    avg_ctrl_peak_coh = mean(ctrl_peak_coh);
+    norm_ps_peak_dt = ps_peak_dt - avg_ctrl_peak_dt;
+    norm_ps_peak_coh = ps_peak_coh - avg_ctrl_peak_coh;
+    norm_gs_peak_dt = gs_peak_dt - avg_ctrl_peak_dt;
+    norm_gs_peak_coh = gs_peak_coh - avg_ctrl_peak_coh;
+    norm_an_peak_dt = an_peak_dt - avg_ctrl_peak_dt;
+    norm_an_peak_coh = an_peak_coh - avg_ctrl_peak_coh;
+    
+    avg_norm_ps_peak_dt = mean(norm_ps_peak_dt);
+    sem_norm_ps_peak_dt = std(norm_ps_peak_dt) ./ sqrt(length(ps_peak_dt));
+    avg_norm_gs_peak_dt = mean(norm_gs_peak_dt);
+    sem_norm_gs_peak_dt = std(norm_gs_peak_dt) ./ sqrt(length(gs_peak_dt));
+    avg_norm_an_peak_dt = mean(norm_an_peak_dt);
+    sem_norm_an_peak_dt = std(norm_an_peak_dt) ./ sqrt(length(an_peak_dt));
+    avg_norm_ps_peak_coh = mean(norm_ps_peak_coh);
+    sem_norm_ps_peak_coh = std(norm_ps_peak_coh) ./ sqrt(length(ps_peak_coh));
+    avg_norm_gs_peak_coh = mean(norm_gs_peak_coh);
+    sem_norm_gs_peak_coh = std(norm_gs_peak_coh) ./ sqrt(length(gs_peak_coh));
+    avg_norm_an_peak_coh = mean(norm_an_peak_coh);
+    sem_norm_an_peak_coh = std(norm_an_peak_coh) ./ sqrt(length(an_peak_coh));
+    [~, p_cgs_ags_dt] = ttest2(abs(norm_gs_peak_dt), abs(norm_an_peak_dt));
+    [~, p_cgs_ags_coh] = ttest2(abs(norm_gs_peak_coh), abs(norm_an_peak_coh));
+    [~, p_ps_cgs_dt] = ttest2(norm_ps_peak_dt, norm_gs_peak_dt);
+    [~, p_ps_cgs_coh] = ttest2(norm_ps_peak_coh, norm_gs_peak_coh);
+    
+    
+    disp("DECISION TIME")
+    fprintf([...
+        'PS and CGS decreased peak decision time by %0.2f +/- %0.2fs and ', ...
+        '%0.2f +/- %0.2fs respectively. \n'], avg_norm_ps_peak_dt, ...
+        sem_norm_ps_peak_dt, avg_norm_gs_peak_dt, sem_norm_gs_peak_dt)
+    fprintf([...
+        'PS and CGS decreased peak coherence by %0.1f +/- %0.1f%% and ', ...
+        '%0.1f +/- %0.1f%% respectively. \n'], avg_norm_ps_peak_coh*100, ...
+        sem_norm_ps_peak_coh*100, avg_norm_gs_peak_coh*100, sem_norm_gs_peak_coh*100)
+    fprintf([...
+        'AGS increased peak decision time by %0.2f +/- %0.2f. \n'], ...
+        avg_norm_an_peak_dt, sem_norm_an_peak_dt)
+    fprintf([...
+        'AGS increased peak coherence by %0.1f +/- %0.1f%%. \n'], ...
+        avg_norm_an_peak_coh*100, sem_norm_an_peak_coh*100)
+    fprintf([...
+        'AGS shifted peak decision time (p=%0.2f) and coherence (%0.1e) ', ...
+        'less than CGS. \n'], p_cgs_ags_dt, p_cgs_ags_coh)
+    fprintf([...
+        'PS and CGS induced statistically equivalent effects on ', ...
+        'peak decision time (p=%0.2f) and peak coherence (p=%0.2f). \n'], ...
+        p_ps_cgs_dt, p_ps_cgs_coh)
     
     %No-decisions
     figure;
